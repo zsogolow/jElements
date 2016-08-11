cf30da450ed2f579f468cdfab0ee05d7e63fc455
Zachery.Sogolow <zachsogolow@Msn.com>
2016-08-11 13:21:28 -0700

    booya

--- test/js/main.js
+++ test/js/main.js
@@ -3,12 +3,14 @@ document.addEventListener('DOMContentLoaded',function() {
    var _bodyNav = _('#bodyNav');
    _menuToggle.bind('click', function() {
        _bodyNav.toggleClass('open');
+       _menuToggle.toggleClass('open');
    });
 
    _(window).bind('resize', function(event) {
        var width = window.innerWidth / 16;
        if (width >= 38) {
            _bodyNav.removeClass('open');
-       }
+           _menuToggle.removeClass('open');
+    }
    });
 });
