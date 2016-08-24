d9bf6dff27440f2c71c5308e20fbd5f2c150bccf
zsogolow <zachsogolow@msn.com>
2016-08-23 19:47:22 -0700

    added support for ejs view templates

--- test/server/static.js
+++ test/server/static.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var fs = require('fs');
+var ejs = require('ejs');
 
 function Static() {
 };
@@ -9,14 +10,27 @@ Static.prototype = {
     serve: function (req, res, done) {
         var contentType = req.headers.accept;
         var path = './' + req.url;
-        fs.readFile(path, function (error, content) {
-            if (error) {
-                done(error);
-            } else {
-                res.writeHead(200, { 'Content-Type': contentType });
-                res.end(content, 'utf-8');
-            }
-        });
+
+        // check for .ejs extension
+        if (req.url.endsWith('.ejs')) {
+            ejs.renderFile(path, function (error, result) {
+                if (error !== null) {
+                    done(error);
+                } else {
+                    res.end(result);
+                }
+            });
+        } else {
+            // serve the static file 
+            fs.readFile(path, 'utf-8', function (error, content) {
+                if (error) {
+                    done(error);
+                } else {
+                    res.writeHead(200, { 'Content-Type': contentType });
+                    res.end(content, 'utf-8');
+                }
+            });
+        }
     },
 };
 
